522988b47046cd919ef888ef23a19551b5212ecd
Sebastian Skerhut <Sebastian.Skerhut@csra.com>
2017-05-15 09:24:34 -0500

    Refactored HeroList details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HerolistComponent } from './components/herolist/herolist.component';
 import { HerodetailsComponent } from './components/herodetails/herodetails.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AppRoutingModule } from './app-routing.module';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,11 +22,7 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot([
-                          {path:'heroes',component:HeroesComponent}
-                          ,{path:'dashboard',component:DashboardComponent}
-                          ,{path:'detail/:id',component:HerodetailsComponent}])
-  ],
+    AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/herolist/herolist.component.ts
+++ src/app/components/herolist/herolist.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Hero } from '../../hero';
 import { HeroService } from '../../services/hero.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-herolist',
   templateUrl: './herolist.component.html',
@@ -13,7 +14,9 @@ export class HerolistComponent implements OnInit {
     onSelected: EventEmitter<Hero> = new EventEmitter<Hero>();
     
   heros:Hero[];
-  constructor(private heroService: HeroService) {
+  constructor(
+          private router: Router,
+          private heroService: HeroService) {
       //this.heros = heroService.getHeroes();
   }
   
@@ -21,9 +24,13 @@ export class HerolistComponent implements OnInit {
       //this.heros = this.heroService.getHeroes();
       this.heroService.getHeroes().then(heroes => this.heros = heroes);
   }
+  gotoDetail() : void {
+      //this.onSelected.emit(this._selectedHero);
+      this.router.navigate(['/detail',this._selectedHero.id]);
+  }
   
   onSelect(hero: Hero): void {
-      this.onSelected.emit(hero);
+      //this.onSelected.emit(hero);
       this._selectedHero = hero;
   }
   
